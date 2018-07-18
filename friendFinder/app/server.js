bfd9841bbe86b427a23c3a8ae31e278adf365e27
colin <colin.rhys.richards@gmail.com>
2018-07-18 13:27:35 -0600

    routes

--- friendFinder/app/server.js
+++ friendFinder/app/server.js
@@ -1,26 +1,40 @@
-// Require/import the HTTP module
-var http = require("http");
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 var express = require("express");
+var bodyParser = require("body-parser");
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
 
-// Define a port to listen for incoming requests
-var PORT = 8070;
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 7777;
 
-// Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
-}
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT:http://localhost:" + PORT);
 });
