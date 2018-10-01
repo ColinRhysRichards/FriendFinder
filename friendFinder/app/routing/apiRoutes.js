c48cc531d8b71dad4aaa721e1d7a2f2478ae1d95
colin <colin.rhys.richards@gmail.com>
2018-10-01 09:54:26 -0600

    cleaned up the code

--- friendFinder/app/routing/apiRoutes.js
+++ friendFinder/app/routing/apiRoutes.js
@@ -1,71 +1,37 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var friendArray = require("../data/friends");
 var path = require("path");
-// var waitListData = require("../data/waitinglistData");
 
+module.exports = function (app) {
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendArray);
   });
 
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
+  app.post("/api/friends", function (req, res) {
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the friendArray array)
-  // ---------------------------------------------------------------------------
-
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
     var friendScore = req.body.scores;
     var scores = [];
     var friendsCount = 0;
     var Match = 0;
-    for (i = 0; i < friendArray.length; i++) { 
-       var diffScores = 0;
+    for (i = 0; i < friendArray.length; i++) {
+      var diffScores = 0;
 
-    for(var j=0; j<friendScore.length; j++){
-      diffScores += (Math.abs(parseInt(friendArray[i].scores[j])-parseInt(friendScore[j])));
+      for (var j = 0; j < friendScore.length; j++) {
+        diffScores += (Math.abs(parseInt(friendArray[i].scores[j]) - parseInt(friendScore[j])));
+      }
+      scores.push(diffScores);
     }
-    scores.push(diffScores);
-    } 
 
-    for(var i=0; i<scores.length; i++){
-      if(scores[i] <= scores[Match]){
+    for (var i = 0; i < scores.length; i++) {
+      if (scores[i] <= scores[Match]) {
         Match = i;
       }
     }
-var finalFriend = friendArray[Match];
-console.log(finalFriend)
-res.json(finalFriend);
+    var finalFriend = friendArray[Match];
+    console.log(finalFriend)
+    res.json(finalFriend);
 
-friendArray.push(req.body);
+    friendArray.push(req.body);
 
   });
 };
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
--- friendFinder/app/server.js
+++ friendFinder/app/server.js
@@ -1,39 +1,18 @@
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
 var app = express();
 app.use(express.static('public'))
 app.use(express.static('images'))
 
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 7777;
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
 
 app.listen(PORT, function() {
   console.log("App listening on PORT:http://localhost:" + PORT);
